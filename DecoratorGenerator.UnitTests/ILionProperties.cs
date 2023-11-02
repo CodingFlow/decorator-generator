356a42213e6c9cd09f3f6249b24b76c7ab45aa45
Leopoldo Fu <3643313+CodingFlow@users.noreply.github.com>
2023-11-02 18:22:45 -0400

    Fix bug: interface with get only property generates setter (#40)
    * Add failing unit test for interface with get only property.
    
    * Fix bug when interface property has get-only, but still generates setter. By checking if interface had getter or setter before adding it to generated decorator abstract class.
    
    * Add additional property get/set combinations for unit test.

---
+++ DecoratorGenerator.UnitTests/ILionProperties.cs
@@ -0,0 +1,13 @@
+﻿using DecoratorGenerator;
+
+namespace SampleLibrary;
+
+[Decorate]
+public interface ILionProperties
+{
+    string Name { get; }
+
+    string Color { set; }
+
+    int Age { get; set; }
+}
