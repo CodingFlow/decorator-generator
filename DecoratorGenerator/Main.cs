fc633192e40cc63d76482f3ce9d6117107ab12ec
CodingFlow <3643313+CodingFlow@users.noreply.github.com>
2025-11-24 20:20:56 -0500

    Update Main.cs to use and implement IIncrementalGenerator interface.

--- DecoratorGenerator/Main.cs
+++ DecoratorGenerator/Main.cs
@@ -1,71 +1,41 @@
 ﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Text;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
+using System.Threading;
 
 namespace DecoratorGenerator
 {
     [Generator]
-    public class Main : ISourceGenerator
+    public class Main : IIncrementalGenerator
     {
-        public void Initialize(GeneratorInitializationContext context) {
-            // No initialization required for this one
+        public void Initialize(IncrementalGeneratorInitializationContext context) {
+            var types = context.SyntaxProvider.ForAttributeWithMetadataName(
+                $"DecoratorGenerator.{nameof(DecorateAttribute)}",
+                predicate: IsSyntaxTargetForGeneration,
+                transform: GetSemanticTargetForGeneration
+            );
+
+            context.RegisterSourceOutput(types, Execute);
         }
 
-        public void Execute(GeneratorExecutionContext context) {
-            var types = GetAllDecoratedTypes(context.Compilation.Assembly.GlobalNamespace);
-
-            var wrapperSymbolWithoutNamespace = context.Compilation.Assembly.GetTypeByMetadataName("WrapperList");
-            var wrapperListTypes =
-                (wrapperSymbolWithoutNamespace == null)
-                ? GetAllTypes(context.Compilation.Assembly.GlobalNamespace, x => x.Name == "WrapperList")
-                : new[] { wrapperSymbolWithoutNamespace };
-            var thirdPartyTypes =
-                wrapperListTypes.SelectMany(x => x.GetMembers()
-                .Where(m => m.Name != ".ctor")
-                .Select(m => m as IFieldSymbol)
-                .Select(f => f.Type)
-                .Select(t => t as INamedTypeSymbol));
-
-            types = types.Concat(thirdPartyTypes);
-
-            var outputs = types.Select(OutputGenerator.GenerateOutputs);
-
-            foreach (var (source, className) in outputs) {
-                // Add the source code to the compilation
-                context.AddSource($"{className}.generated.cs", SourceText.From(source, Encoding.UTF8, SourceHashAlgorithm.Sha256));
-            }
+        private static bool IsSyntaxTargetForGeneration(SyntaxNode syntaxNode, CancellationToken token) {
+            return syntaxNode is InterfaceDeclarationSyntax;
         }
 
-        /// <summary>
-        /// Gets all Types inside the namespace matching the predicate including nested namespaces.
-        /// </summary>
-        /// <param name="input"></param>
-        /// <param name="predicate"></param>
-        /// <returns></returns>
-        private IEnumerable<INamedTypeSymbol> GetAllTypes(INamespaceSymbol input, Func<INamedTypeSymbol, bool> predicate) {
-            foreach (var space in input.GetNamespaceMembers()) {
-                foreach (var item in space.GetTypeMembers()) {
-                    if (predicate(item)) {
-                        yield return item;
-                    }
-                }
+        private static INamedTypeSymbol GetSemanticTargetForGeneration(GeneratorAttributeSyntaxContext context, CancellationToken cancellationToken) {
+            var intefaceSyntax = context.TargetNode as InterfaceDeclarationSyntax;
+            var symbol = context.SemanticModel.GetDeclaredSymbol(intefaceSyntax, cancellationToken: cancellationToken) as INamedTypeSymbol;
 
-                foreach (var nestedItem in GetAllTypes(space, predicate)) {
-                    yield return nestedItem;
-                }
-            }
+            return symbol;
         }
 
-        /// <summary>
-        /// Gets all Types in the namespace decorated with the <see cref="DecorateAttribute"/> including nested namespaces.
-        /// </summary>
-        /// <param name="input"></param>
-        /// <returns></returns>
-        private IEnumerable<INamedTypeSymbol> GetAllDecoratedTypes(INamespaceSymbol input) {
-            return GetAllTypes(input, (x) => x.GetAttributes().Any(att => att.AttributeClass.Name == nameof(DecorateAttribute)));
+        private static void Execute(SourceProductionContext context, INamedTypeSymbol typeSymbol) {
+            var (source, className) = OutputGenerator.GenerateOutputs(typeSymbol);
+
+            context.AddSource($"{className}.generated.cs", SourceText.From(source, Encoding.UTF8, SourceHashAlgorithm.Sha256));
         }
     }
 }
